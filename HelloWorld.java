529fc9f0938bd701e41e2fe3d33b4d92a5780f91
wlh <812799024@qq.com>
2015-06-23 11:02:26 +0800

    1

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Map<String,String> m=new HashMap<String,String>();
+		m.put("key", "value");
+		String s=m.get("key");
+		System.out.println(s);//
+	}
+
+}
