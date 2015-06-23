a84b88e8777a6e057b535a5da43d1d241598f9a2
wlh <812799024@qq.com>
2015-06-23 11:07:05 +0800

    2

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Map<String,String> m=new HashMap<String,String>();
+		Map<String,String> m=new HashMap<String,String>(); //泛型的使用
 		m.put("key", "value");
 		String s=m.get("key");
 		System.out.println(s);//
